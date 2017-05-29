32d0dc62242230f06d031b92eea242c04ac4fe52
yuri-val <yuri.valigursky@gmail.com>
2017-05-29 11:28:01 +0300

    Rate Limiting and Throttling

--- config/application.rb
+++ config/application.rb
@@ -28,6 +28,8 @@ module PageMonitoringSystem
       end
     end
 
+    config.middleware.use Rack::Attack
+    
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
